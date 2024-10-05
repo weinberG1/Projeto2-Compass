4bf21782e465e032cc8717756ce5e66fdd79d865
Paulo Weinberger <pauloweinberger1@icloud.com>
2024-10-05 20:20:51 -0300

    atualizacao user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -13,13 +13,12 @@ curl -L https://github.com/docker/compose/releases/latest/download/docker-compos
 chmod +x /usr/local/bin/docker-compose
 mv /usr/local/bin/docker-compose /bin/docker-compose
 
+mkdir -p /efs/paulo-weinberger/var/www/html
 curl -sL https://raw.githubusercontent.com/weinberG1/Projeto2-Compass/main/docker-compose.yml --output /home/ec2-user/docker-compose.yml
 
-mkdir -p /efs/paulo-weinberger/var/www/html
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-082721f45bb18c4f6.efs.us-east-1.amazonaws.com:/ efs
+sudo chown ec2-user:ec2-user /efs
 
-sudo mount -t efs fs-00551b6438692354b.efs.us-east-1.amazonaws.com:/ /efs
-sudo chown ec2-user:ec2-user /mnt/efs
-
-echo "fs-00551b6438692354b.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
+echo "fs-082721f45bb18c4f6.efs.us-east-1.amazonaws.com:/ efs nfs defaults 0 0" >> /etc/fstab
 
 docker-compose -f /home/ec2-user/docker-compose.yml up -d
