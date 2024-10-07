c4d48a5b56a44f49ad76dcb7008bc1ee95d4ce24
Paulo Weinberger <pauloweinberger1@icloud.com>
2024-10-07 09:08:08 -0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -13,7 +13,7 @@ curl -L https://github.com/docker/compose/releases/latest/download/docker-compos
 chmod +x /usr/local/bin/docker-compose
 mv /usr/local/bin/docker-compose /bin/docker-compose
 
-mkdir -p /efs/wordpress:/var/www/html
+mkdir -p /efs/wordpress/var/www/html
 curl -sL https://raw.githubusercontent.com/weinberG1/Projeto2-Compass/main/docker-compose.yml --output /home/ec2-user/docker-compose.yml
 
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-082721f45bb18c4f6.efs.us-east-1.amazonaws.com:/ efs
