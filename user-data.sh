944d00bdfbdafbce8e05da0a3f992c2c355705f7
Paulo Weinberger <pauloweinberger1@icloud.com>
2024-10-08 11:53:47 -0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -15,26 +15,27 @@ sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 sudo mv /usr/local/bin/docker-compose /bin/docker-compose
 
-# Montando o efs
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0ae75c1b0a5f7958e.efs.us-east-1.amazonaws.com:/ /mnt/efs
-sudo chown ec2-user:ec2-user /mnt/efs
-
 # Criando o diretório para o wordpress no efs
 cd /mnt
 sudo mkdir efs
 cd efs
 sudo mkdir wordpress
 
+# Montando o efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-03fea74ec7e0948bb.efs.us-east-1.amazonaws.com:/ /mnt/efs
+sudo chown ec2-user:ec2-user /mnt/efs
+
+
 # Instalando o arquivo do yml do docker-compose do meu proprio repo no github
 sudo curl -sL https://raw.githubusercontent.com/weinberG1/Projeto2-Compass/main/docker-compose.yml --output /home/ec2-user/docker-compose.yml
 
 # Habilitando a montagem do efs sempre que o sistema for reiniciado 
-echo "fs-0ae75c1b0a5f7958e.efs.us-east-1.amazonaws.com:/ efs nfs defaults 0 0" | sudo tee -a /etc/fstab
+echo "fs-03fea74ec7e0948bb.efs.us-east-1.amazonaws.com:/ efs nfs defaults 0 0" | sudo tee -a /etc/fstab
 
 sudo yum install mysql -y
 
 # Verificacao se o banco de dados ja existe
-sudo mysql --host="database-1.cr0o8ky00uod.us-east-1.rds.amazonaws.com" --user="teste" --password="teste123" --execute="CREATE DATABASE IF NOT EXISTS database1;"
+sudo mysql --host="database1.cr0o8ky00uod.us-east-1.rds.amazonaws.com" --user="teste" --password="teste123" --execute="CREATE DATABASE IF NOT EXISTS database1;"
 
 # Subindo o container do docker-compose
 cd /home/ec2-user
